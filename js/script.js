8cb08e41e2a6de212972b727c9a86aeaa3f74085
wemersonsantos <wemersonsantoseng@gmail.com>
2021-05-05 20:11:18 -0300

    No momento a formatação do editor será deixada de lado para focar no Javascript

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const bottomMenu = document.querySelectorAll(".nav-item");
+const textFormat = document.querySelector("#textFormat").value;
+
+bottomMenu.forEach(bottom => {
+    bottom.addEventListener('click', e =>{
+        console.log("dalle");
+    });
+});
+
+
+
+
+
