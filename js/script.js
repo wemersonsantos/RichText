6bf90346b0f289b7582cc06019e3744441257744
wemersonsantos <wemersonsantoseng@gmail.com>
2021-05-06 21:51:33 -0300

    O javaScript talvez possa ser melhorado e focar mais na questão do layout da página

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
-const bottomMenu = document.querySelectorAll(".nav-item");
-formatar.document.designMode = "on";
+const buttonMenu = document.querySelectorAll(".buttonElements");
+format.document.designMode = "on"; //Faz com que o ducomento possa ser editado.
 
-
-bottomMenu.forEach(bottom => {
-    bottom.addEventListener('click', e =>{
-        formatar.document.execCommand("bold", false, null);
-    });
-});
-
-
-
-
-
-
+buttonMenu.forEach(button => {
+    button.addEventListener('click', e =>{
+        switch (button.dataset.cod){ //O DataSet do button
+            case "formatBlock":
+            format.document.execCommand("formatBlock", false, 'h1');//Os efeitos para serem aplicados
+            break;
+            case "bold":
+            format.document.execCommand("bold", false, null);
+            break;
+            case "italic":
+            format.document.execCommand("italic", false, null);
+            break;
+            case "strikeThrough":
+            format.document.execCommand("strikeThrough", false, null);
+            break;
+            case "underline":
+            format.document.execCommand("underline", false, null);
+            break;
+            case "link":
+            let linkURL = prompt('To insert the URL:' , 'https://');    
+            format.document.execCommand("createLink", false, linkURL);
+            break;
+            case "UnorderedList":
+            format.document.execCommand("insertUnorderedList", false, null);
+            break;
+            case "OrderedList":
+            format.document.execCommand("insertOrderedList", false, null);
+            break;   
+        };//END SWITCH
+        
+    });//END ADDLISTENEREVENT
+});//END FOREACH
 
